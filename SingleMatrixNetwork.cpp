2bfc48c1ee1b7f847ece98fb5ed8731a599d949b
Louis Barbero <louie7195@gmail.com>
2019-04-16 05:22:41 -0400

    WilsonOscillator V2

--- SingleMatrixNetwork.cpp
+++ SingleMatrixNetwork.cpp
@@ -19,46 +19,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+
     Network Willy("Wilson.txt");
-    Willy.cycleNetwork();
+    for(int i = 0; i< 300; i++)
+    Willy.wilsonCycleNetwork();
 
-    /*
-        Below is the matrix multiplication hard coded without the use of the neural network
-    */
-    /*
-    double matrix[4][4] = { {0.9,0.0,-0.95,0.0},
-                            {0.0,0.9,0.0,-0.95},
-                            {0.2,0.0,0.4,-0.5},
-                            {0.0,0.2,-0.5,0.4}
-                        };
 
-    for(int i=0; i<4; i++){
-            cout<<endl;
-        for(int k =0; k<4; k++){
-            cout << matrix[i][k] << " ";
-        }
-    }
-    cout << endl;
-     double outputmatrix[4] = { 0,0,1,0};
-     double resultmatrix[4] = { 0,0,0,0};
-
-    double a=0;
-
-    for(int l = 0; l<50; l++){
-        cout<< l << endl;
-    for(int i=0; i<4; i++){
-        for(int k =0; k<4; k++){
-        resultmatrix[i]+= (matrix[i][k])*(outputmatrix[k]);
-        }}
-
-    for(int i=0; i<4; i++){
-        outputmatrix[i]=resultmatrix[i];
-        resultmatrix[i]=0;
-
-    }
-        cout << setprecision(4);
-        cout << "Motor A:" <<outputmatrix[2] + .5 << "     " << "Motor B:" <<outputmatrix[3] +.5 << endl;
-    } */
 	return 0;
 }
 
